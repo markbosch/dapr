667fc40e76bf9d94a8e15e77dcb02183caa28879
Mark van den Bosch <markvandenbosch@gmail.com>
2023-12-08 16:18:41 +0100

    Dapr service-to-service invocation sample with gRPC

---
+++ README.md
@@ -0,0 +1,6 @@
+# Dapr playground
+
+Repository for [Dapr](https://dapr.io) related stuff.
+
+- Service-to-Service invocation with gRPC
+
---
+++ samples/service_to_service/grpc/GrpcGreeterClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcGreeter;
+
+using static System.Console;
+
+// The port number must match the port of the gRPC server.
+var port = Environment.GetEnvironmentVariable("DAPR_GRPC_PORT") ?? "5062";
+var address = $"http://localhost:{port}";
+WriteLine($"Connecting to {address}...");
+using var channel = GrpcChannel.ForAddress(address);
+var client = new Greeter.GreeterClient(channel);
+
+var metadata = new Metadata { { "dapr-app-id", "server" } };
+var reply = await client.SayHelloAsync(
+                  new HelloRequest { Name = "GreeterClient" }, metadata);
+WriteLine("Greeting: " + reply.Message);
